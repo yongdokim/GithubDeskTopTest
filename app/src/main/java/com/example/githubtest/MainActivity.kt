017f5b66b972e706655a46d38fd589b97db4b344
nbmgpair@gmail.com <64875434+yongdokim@users.noreply.github.com>
2020-05-12 09:33:01 +0900

    if

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    private var isTest:Boolean?=null
+    private var isTest=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        isTest=true;
+        if(isTest){
+
+        }else{
+
+        }
 
 
     }
