bf751311a33856cf0cef5e95fb1296f737f302c6
nbmgpair@gmail.com <64875434+yongdokim@users.noreply.github.com>
2020-05-12 09:23:43 +0900

    inital commit

---
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.githubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private var isTest:Boolean?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
